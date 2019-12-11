c6abb9d6d35f6f6bbdc5480b65f9f7191d98ee6e
Michal_Marszalek <1aam2am1@gmail.com>
2019-12-11 22:03:06 +0100

    Added time measurement

--- include/time/measure_time.c
+++ include/time/measure_time.c
@@ -3,3 +3,41 @@
 //
 
 #include "measure_time.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#if F_CPU != 8000000
+#define TIMER_CMP ((F_CPU * 0.001) / 64)
+#else
+#define TIMER_CMP 125
+#endif
+
+volatile uint16_t time_ms;
+
+void TIMER_Init() {
+    TCCR0 |= (1 << WGM01); //CTC MODE compare
+    TIMSK |= (1 << OCIE0); //enable compare interruption
+
+    TCNT0 = 0; //reset counter
+
+    OCR0 = TIMER_CMP; //1ms TIMER_CMP
+}
+
+void TIMER_start() {
+    TCNT0 = 0; //reset counter
+    time_ms = 0;
+    TCCR0 |= (1 << CS01) | (1 << CS00); //64 prescaler
+}
+
+void TIMER_stop() {
+    TCCR0 &= ~((1 << CS02) | (1 << CS01) | (1 << CS00)); //64 prescaler
+}
+
+uint16_t TIMER_get() {
+    return time_ms;
+}
+
+//compare match 1ms
+ISR(TIMER0_COMP_vect) {
+    ++time_ms;
+}
--- include/time/measure_time.h
+++ include/time/measure_time.h
@@ -5,4 +5,14 @@
 #ifndef REFLEX_TESTER_MEASURE_TIME_H
 #define REFLEX_TESTER_MEASURE_TIME_H
 
+#include <stdint.h>
+
+void TIMER_Init();
+
+void TIMER_start();
+
+void TIMER_stop();
+
+uint16_t TIMER_get();
+
 #endif //REFLEX_TESTER_MEASURE_TIME_H
